2b0c8157ba07c0012c8fc1fb5f884848b47375ca
Gerson Luiz Ferreira Junior <gersonjnr@gmail.com>
2023-04-01 14:49:48 -0300

    PK02 - Organização da estratura do projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'pokemon-list',
+    loadChildren: () =>
+      import('./features/pokemon-list/pokemon-list.module').then(
+        (m) => m.PokemonListModule
+      ),
+  },
+  {
+    path: 'pokemon-detail',
+    loadChildren: () =>
+      import('./features/pokemon-detail/pokemon-detail.module').then(
+        (m) => m.PokemonDetailModule
+      ),
+  },
+  {
+    path: '',
+    redirectTo: '/pokemon-list',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
